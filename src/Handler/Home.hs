010f3ffdcd74aa0358c1307ce685755a12d2cdfb
Andrei Altamira <andrei.altamira@gmail.com>
2021-05-13 21:21:42 -0300

    arquivos com mudanças HTML

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -7,16 +7,31 @@
 module Handler.Home where
 
 import Import
-import Network.HTTP.Types.Status
-import Database.Persist.Postgresql
+import Text.Lucius
+import Text.Julius
+--import Network.HTTP.Types.Status
+--import Database.Persist.Postgresql
 
 getHomeR :: Handler Html
 getHomeR = do
-	defaultLayout $ do
-		toWidgetHead [lucius|
-			h1{
-				color: red;
-			}
-		|]
-		
-		[whamlet|<h1>OLA O CARALHO!!!|]
+    defaultLayout $ do
+        toWidgetHead $(juliusFile "templates/home.julius")
+        toWidgetHead $(luciusFile "templates/home.lucius") 
+        $(whamletFile "templates/home.hamlet")
+        
+getPage1R :: Handler Html
+getPage1R = do
+    defaultLayout $ do
+        $(whamletFile "templates/page1.hamlet")
+        
+getPage2R :: Handler Html
+getPage2R = do
+    defaultLayout $ do
+        $(whamletFile "templates/page2.hamlet")
+        
+getPage3R :: Handler Html
+getPage3R = do
+    defaultLayout $ do
+        $(whamletFile "templates/page3.hamlet")
+        
+        
